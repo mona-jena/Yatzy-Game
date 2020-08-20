cfd19fcee0f22d7d4442538693b62ee8185b24f1
Mona Jena <mona.jena@myob.com>
2020-08-21 03:31:31 +1200

    first commit

---
+++ YatzyKata/YatzyGame.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YatzyKata
+{
+    public class YatzyGame
+    {
+        public static void Main(string[] args)
+        {
+            YatzyGame player = new YatzyGame();
+            //int result = player.RollDice(args);
+
+            //List<int> rolledNum = dice.RollDice();
+        }
+
+        public List<int> RollDice(List<int> args)
+        {
+            List<int> dices = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                Random rolledNumbers = new Random();
+                int randomNum = rolledNumbers.Next(1, 7);
+                dices.Add(randomNum);
+            }
+
+            CalculateSum(dices);
+            foreach (int i in dices)
+            {
+                Console.Write(i);
+            }
+
+            return dices;
+        }
+
+        public int CalculateSum(List<int> dices)
+        {
+            int sum = 0;
+            foreach (int item in dices)
+            {
+                sum += item;˚
+            }
+            Console.Write("Which numbers would you like to keep? Please write the index of number you want to keep eg 1,2,3 to keep first 3 index");
+            string heldNumbers= Console.ReadLine();
+            string[] eachNumToKeep = heldNumbers.Split(",");
+            foreach (string i in eachNumToKeep)
+            {
+                var stringToNum = int.Parse(eachNumToKeep[i]);
+            }
+;           KeepNum(dices, eachIndexToKeep);
+            return sum;
+        }
+
+        public int[] KeepNum(List<int> dices, int[] heldNumbers)
+        {
+            
+        }
+    }
+
+    // public class RollDice()
+    // {
+    //     static Random rand = new Random();
+    //     int rolledNumber = rand.Next(1,7);
+    //     return rolledNumber;
+}
+
+   
---
+++ YatzyUnitTests/YatzyBasicTests.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using Xunit;
+using YatzyKata;
+
+namespace YatzyUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestToSeeIfFiveNumbersGenerated()
+        {
+            List<int> testNumbers = new List<int>();
+            int[] array = new int[] {3, 4, 5, 5, 2};
+            testNumbers.AddRange(array);
+            int expected = 5;
+            YatzyGame player = new YatzyGame();
+            int result = player.RollDice(testNumbers).Count;
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void TestToSeeIfCalculateSumReturnsSum()
+        {
+            List<int> testNumbers = new List<int>();
+            int[] array = new int[] {3, 4, 5, 5, 2};
+            testNumbers.AddRange(array);
+            int expected = 19;
+            YatzyGame player = new YatzyGame();
+            int result = player.CalculateSum(testNumbers);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void TestIfKeepNumKeepsNumbersOnlySpecifiedByUser()
+        {
+            List<int> testNumbers = new List<int>();
+            int[] array = new int[] {3, 4, 5, 5, 2};
+            testNumbers.AddRange(array);
+            int[] heldNumbers = {1,2,3};
+            int[] expected = {3, 4, 5, 0, 0};
+            YatzyGame player = new YatzyGame();
+            int[] result = player.KeepNum(testNumbers, heldNumbers);
+            Assert.Equal(expected, result);
+        }
+    }
+    
+}
