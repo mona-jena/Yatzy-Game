b311eb8d2d29ad8465b11da7c95f89444a153533
Mona Jena <mona.jena@myob.com>
2020-09-03 11:46:52 +1200

    fixed formatting

---
+++ Player/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using YatzyKata;
+
+namespace YatxyKata
+{
+    class Player
+    {
+        public Player()
+        {
+            IConsole consoleActions = new ConssoleActions();
+            IRandom random = new Rng();
+            YatzyGame player = new YatzyGame(consoleActions, random);
+        }
+    }
+}
